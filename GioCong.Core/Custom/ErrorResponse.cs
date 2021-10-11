383dc58a0009d36b0a4880b9f43c6a0a08c11235
hieu03012000 <hieu03012000@gmail.com>
2021-10-11 10:03:58 +0700

    (hieunt) init: add core library

---
+++ GioCong.Core/Custom/ErrorResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GioCong.Core.Custom
+{
+    public class ErrorResponse : Exception
+    {
+        public ErrorDetailResponse Error { get; private set; }
+
+        public ErrorResponse(int errorCode, string message)
+        {
+            Error = new ErrorDetailResponse
+            {
+                Code = errorCode,
+                Message = message
+            };
+        }
+
+        public ErrorResponse()
+        {
+        }
+    }
+
+    public class ErrorDetailResponse
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
